bf99f840cd83ee8f886c96737ed97fd3c54e4b09
Nat Lasseter <user@4574.co.uk>
2022-12-02 10:37:10 +0000

    Day 01

---
+++ day01/part1.rb
@@ -0,0 +1,15 @@
+items = $stdin.readlines.map(&:strip)
+elves = []
+
+sum = 0
+items.each do |item|
+  if item.empty?
+    elves << sum
+    sum = 0
+  else
+    sum += item.to_i
+  end
+end
+elves << sum
+
+puts elves.max
